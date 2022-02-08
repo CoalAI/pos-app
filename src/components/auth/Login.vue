2a7da7b567fc324a828f2f57481ea9ea7bbec7a9
aqsawaheed-8840 <aqsawaheed8840@gmail.com>
2022-02-08 20:55:50 +0000

    feat(login): upgrade login page

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,45 +1,58 @@
-<template >  
-  <div>
-    <div class="header">
+<template>  
+  <div class="header">
       <h1 class="text-center"><img src="../../assets/logo.png" alt="coaldev" class="img-center"></h1>  
-    </div>
-    <div class="wrapper mr-6">
+  </div>
+  <div>
+    <!--<div class="wrapper"-->
+    <div style="background:url(../../assets/login-back.png)">
+      <h1 class="head">POS</h1>    
       <div class="col">
+        <img src="" class="user"/>
         <form method="POST" @submit="(e) => e.preventDefault()">
           <div class="flex-box">
             <!--<label class="pad-label" for="username">
               <strong>Username:</strong>
             </label>-->
+            <button class="btn-search btn-orange">
+              <img src="" alt="Search">
+            </button>
             <input
+              class="search-input search-box"
               v-model="username"
               label="Username"
               name="username"
               type="text"
-              placeholder="Enter Username"
+              placeholder="Username"
               required="True"
-            />
+            />            
+            
           </div>
           <div class="flex-box">
             <!--<label class="pad-label" for="password">
               <strong>Password:</strong>
             </label>-->
+            <button class="btn-search btn-orange">
+              <img src="" alt="Search">
+            </button>
             <input
+              class="search-input search-box"
               v-model="password"
               id="password"
               label="Password"
               name="password"
               type="password"
-              placeholder="Enter Password"
+              placeholder="Password"
               required="True"
             />
+            
           </div>
           <span v-if="loginValidation" class="form-error">{{ loginValidation }}</span>
 
           <input
           type="submit"
-          class="btn btn-orange btn-lg"
+          class="btn-red btn-orange"
           @click="submitLogin"
-          value="login"/>
+          value="Login"/>
         </form>
         <Loader v-show="showLoader"/>
       </div>
@@ -96,22 +109,47 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-
+  body{
+    margin:0;
+    padding:0;
+    background: url(../../assets/login-back.png);
+    background-size: cover;
+    background-position: center;
+  }
   .wrapper {
     display: grid;
     grid-template-columns: repeat(12, 1fr);
     grid-auto-rows: minmax(100px, auto);
     gap: 1em;
+    top:50%;
+    left:50%;
+    position:absolute;
   }
 
   .col {
-    grid-column: 5/10;
+    box-sizing: border-box;
+    top:50%;
+    left:34%;
+    position:absolute;
+    width:500px;
+    background-color:#ffffff;
     border: 1px solid $white-color;
     border-radius: 10px;
-    padding: 4em;
+    padding: 2em;
     -webkit-box-shadow: 1px 1px 10px -1px $login-shodow-color;
     -moz-box-shadow: 1px 1px 10px -1px $login-shodow-color;
     box-shadow: 1px 1px 10px -1px $login-shodow-color;
+  }
+  .col:focus{
+    border:1px solid #e43d2a;
+  }
+  
+  .col:active{
+    border:1px solid #e43d2a;
+  }
+  
+  .col:hover{
+    border:1px solid #e43d2a;
   }
 
   .pad-label {
@@ -134,4 +172,73 @@ export default defineComponent({
     height:100%;
     margin-top:1%;
   }
+  .flex-box{
+    width: 250px;
+    height:50px;
+    
+    
+  }
+  .btn-search {
+    border-radius:20px 0px 0px 20px;
+    color: $white-color;
+    //padding: $normal-btn-pad;
+    margin: $margin-btn-input;
+    cursor: pointer;
+    font-family:seg;
+    font-size: 10px;
+    border: 1px solid #dfe0e1;
+    border-right-style:none!important;
+    text-align: center;
+    width:20%; 
+  }
+  .search-box{
+    border-radius:0px 20px 20px 0px;
+    padding:0px 0px 0px 10px;
+    margin:8px 0px 8px 0px;
+    font-size:14px;
+    color:#9ea3a6;
+    font-family:seg;
+    border: 1px solid #dfe0e1;
+    border-right-style:none !important;
+}
+.btn-red{
+  background-color:#e43d2a;
+  font-family:seg;
+  font-size: 12px;
+  margin:8px 0px 2px 60px;
+  border-radius: 20px;
+  width: 135px;
+  cursor: pointer;
+  color:$white-color;
+  border:none;
+  padding:5px;
+  font-weight:bold;
+}
+.btn-red:focus{
+  outline: none;
+}
+.user{
+  outline:none;
+  border:none;
+  background-color:#0f2634;
+  border-radius:50%;
+  width:70px;
+  height:70px;
+  position:absolute;
+  top:-40px;
+  left:calc(50% - 50px);
+}
+form{
+  margin-left:18%;
+}
+.form-error{
+  font-family:seg;
+  font-size:11px;
+  display:block;
+}
+.head{
+  color:black;
+  font-size:max;
+  font-family:seg;
+}
 </style>
