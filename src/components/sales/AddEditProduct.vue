c815d8d8f880cf13990c054651ba07be711c1eed
1abdurRehman <abdurrehman@coaldev.com>
2022-04-06 13:03:26 +0000

    feat(randomT): testing

--- src/components/sales/AddEditProduct.vue
+++ src/components/sales/AddEditProduct.vue
@@ -488,6 +488,7 @@ export default defineComponent({
     await this.fetchCategories();
     if (this.productId) {
       await this.getProduct(this.productId);
+      console.log(this.singleProduct)
       this.loadData(this.singleProduct);
     }
   },
