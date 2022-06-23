e61afd3e3f8aa1e262601158d59e5d8cd26dcb3f
Khunshan Butt <khunshanbutt@coaldev.com>
2022-06-23 19:41:05 +0500

    fix(chart): chart colours from constant file

--- src/components/analytics/ComparisonAnalysis.vue
+++ src/components/analytics/ComparisonAnalysis.vue
@@ -55,6 +55,7 @@ import { BarChart } from "vue-chart-3";
 import { Chart, registerables } from "chart.js";
 import { store } from "@/store";
 import printBill from '@/utils/print';
+import chartConfig from "@/constants";
 Chart.register(...registerables);
 
 export default defineComponent({
@@ -136,24 +137,8 @@ export default defineComponent({
             data: JSON.parse(
               JSON.stringify(store.getters.getComparisonanalysis)
             ),
-            backgroundColor: [
-              "rgba(255, 99, 132, 0.2)",
-              "rgba(255, 159, 64, 0.2)",
-              "rgba(255, 205, 86, 0.2)",
-              "rgba(75, 192, 192, 0.2)",
-              "rgba(54, 162, 235, 0.2)",
-              "rgba(153, 102, 255, 0.2)",
-              "rgba(201, 203, 207, 0.2)",
-            ],
-            borderColor: [
-              "rgb(255, 99, 132)",
-              "rgb(255, 159, 64)",
-              "rgb(255, 205, 86)",
-              "rgb(75, 192, 192)",
-              "rgb(54, 162, 235)",
-              "rgb(153, 102, 255)",
-              "rgb(201, 203, 207)",
-            ],
+            backgroundColor: chartConfig.backgroundColor,
+            borderColor: chartConfig.borderColor,
           },
         ],
       };
